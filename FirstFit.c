4b8df6ddcac1cd2163be63adabd9d1065949ea7d
DharaneeswaranR <dharaneeshram@gmail.com>
2022-09-26 23:27:56 +0530

    simplified FirstFit.c

--- FirstFit.c
+++ FirstFit.c
@@ -1,55 +1,80 @@
 #include <stdio.h>
 
-void main()
+int main()
 {
-    int bod, np, sb[20], sp[20], z[20], f[20], s = 0, i, j;
-    printf("\nEnter the number of blocks of division of memory: ");
-    scanf("%d", &bod);
-    printf("\nEnter the size of each block: ");
-    for (i = 0; i < bod; i++)
+    int mn, pn;
+    printf("No. of blocks: ");
+    scanf("%d", &mn);
+    printf("No. of process: ");
+    scanf("%d", &pn);
+
+    int m[mn], p[pn], aloc[pn];
+
+    for (int i = 0; i < mn; i++)
     {
-        scanf("%d", &sb[i]);
-        z[i] = sb[i];
-        f[i] = 0;
+        printf("\nSize of memory block %d: ", i);
+        scanf("%d", &m[i]);
     }
-    printf("\nEnter the number process: ");
-    scanf("%d", &np);
-    printf("\nEnter the size of each process: ");
-    for (i = 0; i < np; i++)
+
+    for (int i = 0; i < pn; i++)
     {
-        scanf("%d", &sp[i]);
+        printf("\nSize of process %d: ", i);
+        scanf("%d", &p[i]);
+        aloc[i] = 0;
     }
-    for (i = 0; i < np; i++)
+
+    for (int i = 0; i < pn; i++)
     {
-        for (j = 0; j < bod; j++)
+        for (int j = 0; j < mn; j++)
         {
-            if (sb[j] >= sp[i] && f[j] == 0)
+            if (p[i] <= m[j] && aloc[i] == 0) 
             {
                 printf("\nProcess %d is allocated to block %d", i, j);
-                f[j] = 1;
-                z[j] = sb[j] - sp[i];
-                s++;
-                goto l1;
+                aloc[i] = 1;
+                m[j] = m[j] - p[i]; 
             }
         }
-        printf("\nProcess %d cannot be allocated", i);
-    l1:
-        printf("");
+        
     }
-    printf("\nRemaining space left in each block");
-    for (i = 0; i < bod; i++)
+    
+    printf("\nRemaining space left:\n");
+    for (int i = 0; i < mn; i++)
     {
-        printf("\nBlock %d: %d", i, z[i]);
+        printf("\nBlock %d: %d", i, m[i]);
     }
-    for (i = 0; i < bod; i++)
+    
+     
+    for (int i = 0; i < pn; i++)
     {
-        if (f[i] == 0)
+        if (aloc[i] == 0)
         {
-            printf("\nBlock %d is unallocated", i);
+            printf("\nProcess %d is unallocated", i);
         }
     }
-    if (s == bod)
-    {
-        printf("\nNo block is left unallocated");
-    }
 }
+
+/*
+
+No. of blocks: 3
+
+No. of process: 2
+
+Size of memory block 0: 242
+
+Size of memory block 1: 300
+
+Size of memory block 2: 200
+
+Size of process 0: 100
+
+Size of process 1: 300
+
+Process 0 is allocated to block 0
+Process 1 is allocated to block 1
+Remaining space left:
+
+Block 0: 142
+Block 1: 0
+Block 2: 200
+
+*/

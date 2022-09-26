08af28d1eb8362746add255112b3c1be6609e783
DharaneeswaranR <dharaneeshram@gmail.com>
2022-09-26 17:46:17 +0530

    updated calc.sh

--- Shell Program/calc.sh
+++ Shell Program/calc.sh
@@ -1,13 +1,13 @@
 echo 1.Add 2.Sub 3.Mul 4.Div
-echo Oppr
+echo "Enter the operator"
 read o
-echo Enter a
+echo "Enter the operands"
 read a
-echo Enter b
 read b
 case $o in
 "1")echo "$a + $b = `expr $a + $b`";;
 "2")echo "$a - $b = `expr $a - $b`";;
 "3")echo "$a * $b = `expr $a \* $b`";;
 "4")echo "$a / $b = `expr $a / $b`";;
+* ) echo " Inavlid Operation"
 esac
